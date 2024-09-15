8e195bceab27743f0f494b4c1e5ad454c1c71e35
Ali Valizaadeh <ali.vlizadeh1@gmail.com>
2024-09-16 01:14:14 +0330

    master : each of type project has own branch.

--- src/main/java/com/av/dao/LinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.av.dao;
-
-import com.av.entity.Link;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinkRepository extends MongoRepository<Link, Long>, LinkCustomRepository {
-}
